f3478f3db99b317abf72f8cf5a09aa729ff8151f
joebertbilbao <jbbilbao80@gmail.com>
2024-10-14 10:17:35 +0800

    repodt

--- action/admin.php
+++ action/admin.php
@@ -70,7 +70,7 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
                       Swal.fire({
                           title: 'WARNING! Notify to Admin!',
                           title: 'Security Alert! Access Denied!',
-                          text: 'This Facility Under 24 hours Surveillance ,Trespassers will be prosecuted!!',
+                          text: 'This Facility Under 24 hours Surveillance ,Trespassers will be prosecuted!',
                           icon: 'error',
                           confirmButtonText: 'Try Again'
                 }).then(() => {
