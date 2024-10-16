f57a2caf0c911a992d1b459ef669dfd682eca705
joebertbilbao <jbbilbao80@gmail.com>
2024-10-16 22:32:21 +0800

    asasa

--- portal.php
+++ portal.php
@@ -179,8 +179,6 @@ function isStepReached($step) {
         <img src="assets/image/download.png" alt="Madridejos Community College Logo" width="100" height="80">
         <p>MADRIDEJOS COMMUNITY COLLEGE</p>   
     </div>
-     <a href="./index.php" class="nav-link"><i class="bi bi-house-door"></i> Home</a>
-    
     <a href="admin/index.php" class="nav-link"><i class="bi bi-person-lock"></i>Admin Dashboard</a>
     <a href="guidance/index.php" class="nav-link"><i class="bi bi-calendar-check"></i> Guidance Office</a>
     
@@ -208,7 +206,7 @@ function isStepReached($step) {
     <a href="cor/index.php" class="nav-link"><i class="bi bi-file-earmark-code"></i> COR</a>
     <?php endif; ?>
     
-   
+    <a href="./index.php" class="nav-link"><i class="bi bi-house-door"></i> Back to Home</a>
 </div>
     <div class="main-content">
         <div class="login-box">
