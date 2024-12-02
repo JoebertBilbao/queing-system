0828fb5bc879f88d2023feedc8f5ab7feefa1514
Joerge Yangao <joergeyangao25@gmail.com>
2024-12-02 17:07:21 +0800

    footer

--- index.php
+++ index.php
@@ -198,26 +198,35 @@
 
   <footer id="footer" class="footer">
 
-    <div class="container">
-      <div class="copyright text-center ">
-        <p>© <span>Copyright</span> <strong class="px-1 sitename">MCCQUEUEINGSYSTEM</strong> <span>All Rights Reserved</span></p>
-      </div>
-      <div class="social-links d-flex justify-content-center">
-        <a href=""><i class="bi bi-twitter-x"></i></a>
-        <a href=""><i class="bi bi-facebook"></i></a>
-        <a href=""><i class="bi bi-instagram"></i></a>
-        <a href=""><i class="bi bi-linkedin"></i></a>
-      </div>
-      <div class="credits">
-        <!-- All the links in the footer should remain intact. -->
-        <!-- You can delete the links only if you've purchased the pro version. -->
-        <!-- Licensing information: https://bootstrapmade.com/license/ -->
-        <!-- Purchase the pro version with working PHP/AJAX contact form: [buy-url] -->
-        <!-- Designed by <a href="https://bootstrapmade.com/">BootstrapMade</a> -->
-      </div>
-    </div>
+<div class="container">
+<div class="copyright text-center">
+<p>© <span>Copyright</span> <strong class="px-1 sitename">MCCQUEUEINGSYSTEM</strong> <span>All Rights Reserved</span></p>
 
-  </footer>
+<!-- Contact Information -->
+<div class="contact-info">
+<div class="contact-group">
+  <strong>Web Developer:</strong> <a href="#">Joebert Bilbao</a> |
+  <strong>MCC Page:</strong> <a href="https://www.facebook.com/madridejoscollege" target="_blank">MCC Facebook Page</a>
+</div>
+<div class="contact-group">
+  <strong>Email:</strong> <a href="#">Jbbilbao80@gmail.com</a> |
+  <strong>Contact:</strong> <a href="#">09551349995</a>
+</div>
+<div class="contact-group">
+  <strong>Address:</strong> <a href="#">6053, Purok Mahugany, Sitio Gahung, Brgy Kodia</a> 
+</div>
+</div>
+</div>
+  <div class="credits">
+    <!-- All the links in the footer should remain intact. -->
+    <!-- You can delete the links only if you've purchased the pro version. -->
+    <!-- Licensing information: https://bootstrapmade.com/license/ -->
+    <!-- Purchase the pro version with working PHP/AJAX contact form: [buy-url] -->
+    <!-- Designed by <a href="https://bootstrapmade.com/">BootstrapMade</a> -->
+  </div>
+</div>
+
+</footer>
 
   <!-- Scroll Top -->
   <a href="#" id="scroll-top" class="scroll-top d-flex align-items-center justify-content-center"><i class="bi bi-arrow-up-short"></i></a>
