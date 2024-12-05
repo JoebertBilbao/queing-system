1fdfe5174d11628e097772aa6e1a69f36946fc7a
JoebertBilbao <jbbilbao80@gmail.com>
2024-12-05 21:47:33 +0800

    Update db.php

--- database/db.php
+++ database/db.php
@@ -1,15 +1,15 @@
 <?php
-
-$servername = "127.0.0.1";
+$servername = "127.0.0.1"; // Replace with your hosting's DB hostname
 $username = "u510162695_mccsystem";
 $password = "1Mccsystem";
 $dbname = "u510162695_mccsystem";
 
-// Create connection
+// Create connection with error reporting
 $conn = new mysqli($servername, $username, $password, $dbname);
 
-// Check connection
 if ($conn->connect_error) {
     die("Connection failed: " . $conn->connect_error);
+} else {
+    echo "Connected successfully!";
 }
 ?>
