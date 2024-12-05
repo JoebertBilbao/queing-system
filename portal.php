14c5de7d853ad53aa4229003457d4929e447c763
JoebertBilbao <jbbilbao80@gmail.com>
2024-12-05 22:25:42 +0800

    Update portal.php

--- portal.php
+++ portal.php
@@ -200,9 +200,7 @@ function isStepReached($step) {
         <img src="assets/image/download.png" alt="Madridejos Community College Logo" width="100" height="80">
         <p>MADRIDEJOS COMMUNITY COLLEGE</p>
     </div>
-    
-    <a href="index.php" class="nav-link"><i class="bi bi-house-door"></i>Home</a>
-    
+   
     <a href="admin/verification.php" class="nav-link"><i class="bi bi-person-lock"></i>Admin Dashboard</a>
     <a href="guidance/index.php" class="nav-link"><i class="bi bi-calendar-check"></i> Guidance Office</a>
     
@@ -227,6 +225,9 @@ function isStepReached($step) {
     <?php if (isStepReached('step 7')): ?>
     <a href="cor/index.php" class="nav-link"><i class="bi bi-file-earmark-code"></i> COR</a>
     <?php endif; ?>
+
+    <a href="index.php" class="nav-link"><i class="bi bi-house-door"></i>Home</a>
+ 
 </div>
     <div class="main-content">
         <div class="login-box">
